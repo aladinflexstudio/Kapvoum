a70b542c6e5cdcefa6182c90487620c9ff4f3182
jeanfrancoisaladin-droid <jeanfrancoisaladin@gmail.com>
2026-04-12 12:52:34 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Error');
+      return;
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(data);
+  });
+});
+
+server.listen(process.env.PORT || 3000);
